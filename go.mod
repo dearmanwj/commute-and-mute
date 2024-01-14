1bb37ba4b312598173062731972654c829bdc7dd
William Dearman <williamdearman@hotmail.co.uk>
2024-01-14 19:41:33 +0100

    try to support both http and lambda (not working yet)

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module willd/commute-and-mute
 
 go 1.21.5
 
-require github.com/aws/aws-lambda-go v1.43.0 // indirect
+require github.com/aws/aws-lambda-go v1.43.0
--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	lambda.Start(handler)
+	if len(os.Getenv("_LAMBDA_SERVER_PORT")) > 0 {
+		lambda.Start(handler)
+	} else {
+		http.HandleFunc("/", handlerHttp)
+		http.ListenAndServe(":8080", nil)
+	}
 }
 
 func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
 	log.Println("Hello world")
+	log.Println(request.QueryStringParameters)
+	if request.RequestContext.HTTP.Method == "GET" {
+		response := events.LambdaFunctionURLResponse{
+			StatusCode: 200,
+			Body:       request.QueryStringParameters["hub.challenge"],
+		}
+		return response, nil
+	}
 	response := events.LambdaFunctionURLResponse{
 		StatusCode: 200,
-		Body:       "This is my function",
+		Body:       "Not a get request",
 	}
 	return response, nil
+}
+
+func handlerHttp(w http.ResponseWriter, r *http.Request) {
+	log.Println("In http handler")
+	if r.Method == "GET" {
+		challengeString := r.URL.Query().Get("hub.challenge")
+		fmt.Fprintf(w, challengeString)
+	}
 }
