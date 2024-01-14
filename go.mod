1e5321912cd0d9f8a53e33855e7c3569961865d3
William Dearman <williamdearman@hotmail.co.uk>
2024-01-14 21:19:04 +0100

    create handler for post activity request

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module willd/commute-and-mute
 
 go 1.21.5
-
-require github.com/aws/aws-lambda-go v1.43.0
--- main.go
+++ main.go
@@ -1,39 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
-	if len(os.Getenv("_LAMBDA_SERVER_PORT")) > 0 {
-		lambda.Start(handler)
-	} else {
-		http.HandleFunc("/", handlerHttp)
-		http.ListenAndServe(":8080", nil)
-	}
+type Activity struct {
+	Id           int64
+	Type         string
+	Start_latlng [2]float32
+	End_latlng   [2]float32
 }
 
-func handler(request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
-	log.Println("Hello world")
-	log.Println(request.QueryStringParameters)
-	if request.RequestContext.HTTP.Method == "GET" {
-		response := events.LambdaFunctionURLResponse{
-			StatusCode: 200,
-			Body:       request.QueryStringParameters["hub.challenge"],
-		}
-		return response, nil
-	}
-	response := events.LambdaFunctionURLResponse{
-		StatusCode: 200,
-		Body:       "Not a get request",
-	}
-	return response, nil
+func main() {
+	http.HandleFunc("/", handlerHttp)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handlerHttp(w http.ResponseWriter, r *http.Request) {
@@ -41,5 +24,9 @@ func handlerHttp(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		challengeString := r.URL.Query().Get("hub.challenge")
 		fmt.Fprintf(w, challengeString)
+	} else {
+		var a Activity
+		json.NewDecoder(r.Body).Decode(&a)
+		fmt.Fprintf(w, "Activity: %v", a)
 	}
 }
