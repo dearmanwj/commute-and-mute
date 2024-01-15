9f391fb7492634af9b7fb2bd950f606ff58c5ac0
William Dearman <williamdearman@hotmail.co.uk>
2024-01-15 08:58:26 +0100

    added update activity type and corrected work lat lng

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math"
 	"net/http"
+	"strings"
 )
 
 const HomeLat float64 = 43.593583
 const HomeLng float64 = 1.448228
-const WorkLat float64 = 43.564088
-const WorkLng float64 = 1.389139
-const RadiusKM float64 = 0.025
+const WorkLat float64 = 43.564060
+const WorkLng float64 = 1.389155
+const RadiusKM float64 = 0.1
 
 type Activity struct {
 	Id           int64
 	Type         string
 	Start_latlng [2]float64
 	End_latlng   [2]float64
+}
+
+type ActivityUpdate struct {
+	Commute      bool
+	HideFromHome bool
 }
 
 func main() {
@@ -41,13 +48,18 @@ func handlerHttp(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func ProcessActivity(a Activity) {
-	if a.Type == "ride" {
+func ProcessActivity(a Activity) (err error) {
+	if strings.EqualFold(a.Type, "ride") {
 		log.Println("Received Ride activity")
 		if isCommute(a.Start_latlng[0], a.Start_latlng[1], a.End_latlng[0], a.End_latlng[1]) {
 			log.Println("is ride and commute")
+			toSend := ActivityUpdate{Commute: true, HideFromHome: true}
+			fmt.Printf("To send: %v", toSend)
+		} else {
+			log.Println("ride not between home and work locations")
 		}
 	}
+	return nil
 }
 
 func isCommute(startLat, startLng, endLat, endLng float64) bool {
@@ -73,6 +85,7 @@ func isWithinRadius(lat1, lng1, lat2, lng2 float64) bool {
 		degToRad(lat2),
 		degToRad(lng2),
 	)
+	log.Println(distKm)
 	return distKm < RadiusKM
 }
 
