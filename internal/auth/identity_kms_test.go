99ee3eb1a7db401b3c75368a0a1a9289e34ed5c0
William Dearman <williamdearman@hotmail.co.uk>
2024-04-08 20:10:44 +0200

    added unit test for validate

--- internal/auth/identity_kms_test.go
+++ internal/auth/identity_kms_test.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"encoding/asn1"
+	"encoding/base64"
 	"math/big"
 	"strings"
 	"testing"
@@ -51,6 +52,39 @@ func TestGenerateSigned(t *testing.T) {
 	// Can definitely think of a better assert than this...
 	if len(tokenParts) != 3 {
 		t.Error("incorrect signature")
+	}
+}
+
+func TestValidateToken(t *testing.T) {
+	// Given
+	mock := mockKmsApi{
+		mockSign: nil,
+		mockGetPublicKey: func(ctx context.Context, params *kms.GetPublicKeyInput, optFns ...func(*kms.Options)) (*kms.GetPublicKeyOutput, error) {
+			keyString := "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAETz8/nVxGqJ0QwrDsnuG6EOEEUZ3jQ1rbwJo7G8IJ2zGb8p2Xjrph+90p6T5ityqoYW+inVJ2vh+kmbdb9jzcBA=="
+			keyBytes, _ := base64.StdEncoding.DecodeString(keyString)
+			output := kms.GetPublicKeyOutput{
+				PublicKey: keyBytes,
+			}
+			return &output, nil
+		},
+	}
+
+	token := "eyJhbGciOiJFUzI1NiIsInR5cCI6Imp3dCJ9.eyJzdWIiOjEwNTAzODEyLCJpc3MiOiJjb21tdXRlLWFuZC1tdXRlIiwiZXhwIjoxNzEyNTk5ODI5fQ.p5srwusF05SV_F7Z7p-NTlKJCafL7lYBHNmJ0DeNwyt_0aXXlV6COdI2O6eCs06ZmldyaCYPhc5Uc47jXNtlhA"
+
+	generator := TokenGenerator{
+		client: mock,
+	}
+	ctx := context.Background()
+
+	// When
+	result, err := generator.Validate(ctx, token)
+
+	// Then
+	if err != nil {
+		t.Error("could not validate token")
+	}
+	if result != 10503812 {
+		t.Error("incorrect sub")
 	}
 }
 
