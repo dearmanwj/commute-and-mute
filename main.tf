7d06b6fddaddf0c0ed02ac5e2f498df277f1ece6
William Dearman <williamdearman@hotmail.co.uk>
2024-05-01 16:17:24 +0200

    added terraform to provision authorizer bucket

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.38.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      project = "commute-and-mute"
+    }
+  }
+
+}
+
+resource "aws_s3_bucket" "authorizer_lambda_bucket" {
+  bucket = "commute-and-mute-authorizer-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "authorizer_lambda_bucket" {
+  bucket = aws_s3_bucket.authorizer_lambda_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "authorizer_lambda_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.authorizer_lambda_bucket]
+
+  bucket = aws_s3_bucket.authorizer_lambda_bucket.id
+  acl    = "private"
+}
